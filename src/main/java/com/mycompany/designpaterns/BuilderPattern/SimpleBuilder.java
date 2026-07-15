3759098363260d17c97a98eda25556d857cb927f
NotKnown <smuhammadanser@gmail.com>
2026-07-15 22:41:09 +0500

    added Builder Pattern

---
+++ src/main/java/com/mycompany/designpaterns/BuilderPattern/SimpleBuilder.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.designpaterns.BuilderPattern;
+
+/**
+ *
+ * @author SHAH
+ */
+
+class Pizza{
+
+    private Pizza() {
+    }
+    
+    String crust;
+    String flavour;
+    String toppings;
+    
+    
+    
+}
+
+public class SimpleBuilder {
+    
+}
